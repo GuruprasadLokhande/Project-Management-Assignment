340e39bcf1679ec1ecc807ffd68a1767c7988346
GuruprasadLokhande <gurulokhande2915@gmail.com>
2025-10-30 23:31:09 +0530

    Assignment Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-900 text-white text-center">
+      <div className="text-5xl font-bold mb-4">React App</div>
+      <div className="text-2xl font-medium">
+        Project Management Assignment Setup
+      </div>
+    </div>
+  );
+}
+
+export default App;
